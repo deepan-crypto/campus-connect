9405bd4a110bbe0d0131436d50ca97ee4f7eb999
deepan-crypto <deepang467@gmail.com>
2025-10-07 00:13:34 +0530

    update servr issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      host: 'campus-connect-1-vwmy.onrender.com',
+      protocol: 'wss',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
